665d84fd724272a6529e82089ab5543989c071c3
Aniket Yadav <yaniketofficial@gmail.com>
2026-06-24 12:11:13 +0000

    Vector Problem 2

--- STL Containers/Vector/problem2.cpp
+++ STL Containers/Vector/problem2.cpp
@@ -5,3 +5,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int main() {
+    vector<int> stocksPriceByDay = { 7, 10,  1,  5,  3,  6,  4 };
+    
+    int minPrice = stocksPriceByDay[0];
+    int minPriceDay = 0;
+
+    int maxProfit = 0;
+    int bestSellDay = 0;
+    int bestBuyDay = 0;
+
+    for (int i = 1; i < stocksPriceByDay.size(); i++) {
+        if (stocksPriceByDay[i] < minPrice) {
+            minPrice = stocksPriceByDay[i];
+            minPriceDay = i;
+        }
+        else if (stocksPriceByDay[i] - minPrice > maxProfit) {
+            maxProfit = stocksPriceByDay[i] - minPrice;
+            bestBuyDay = minPriceDay;
+            bestSellDay = i;
+        }
+    }
+
+    cout << "The Best day for the purchase is : " << bestBuyDay+1 << "\n";
+    cout << "The Best day for sale is : " << bestSellDay+1 << "\n";
+    cout << "Total profit for the trade is : " << maxProfit << "\n";
+
+    return 0;
+}
